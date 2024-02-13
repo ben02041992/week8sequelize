2c4ae8727e3ee85d14d429583a392eef26815fb6
Ben Townsend <benjamintownsend432@gmail.com>
2024-02-13 11:49:48 +0000

    Enhancement: add server health and working

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.send({ message: "API is healthy" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}.`);
 });
