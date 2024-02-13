f5a20bc8cdd774c822803c58e1508067a462ddf5
Ben Townsend <benjamintownsend432@gmail.com>
2024-02-13 14:05:08 +0000

    enhancement: remove timestamps

--- src/books/model.js
+++ src/books/model.js
@@ -2,20 +2,24 @@ const { DataTypes } = require("sequelize");
 
 const sequelize = require("../db/connection");
 
-const Book = sequelize.define("Book", {
-  title: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
+const Book = sequelize.define(
+  "Book",
+  {
+    title: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      defaultValue: "Some Author",
+    },
+    genre: {
+      type: DataTypes.STRING,
+      defaultValue: "Some genre",
+    },
   },
-  author: {
-    type: DataTypes.STRING,
-    defaultValue: "Some Author",
-  },
-  genre: {
-    type: DataTypes.STRING,
-    defaultValue: "Some genre",
-  },
-});
+  { timestamps: false }
+);
 
 module.exports = Book;
